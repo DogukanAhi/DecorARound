e64e32956dfb61bab54f18a7bbc12de137c3c82c
DogukanAhi <bf427@hotmail.com>
2025-05-17 17:11:38 +0300

    finalize app

---
+++ DecorARound/Cells/BestSellerCell.swift
@@ -0,0 +1,10 @@
+
+
+import UIKit
+
+class BestSellerCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productNameLbl: UILabel!
+    
+}
