0f196d27e7e03d4f6cd17edf73d5abf80290fd7f
DogukanAhi <bf427@hotmail.com>
2025-05-27 17:53:06 +0300

    finalize arVC

---
+++ DecorARound/View/BestSellerHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  BestSellerHeaderView.swift
+//  DecorARound
+//
+//  Created by Doğukan Ahi on 17.05.2025.
+//
+
+import UIKit
+
+class BestSellerHeaderView: UICollectionReusableView {
+
+    @IBOutlet weak var headerLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
